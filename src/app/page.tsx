119357111d5b4196e0a9161b666773762818ace5
ashrafali2000 <ali4282271@gmail.com>
2024-06-03 14:53:10 +0500

    add  use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Footer from "@/components/footer";
+"use client";
 import DevelopmentServices from "@/components/home/developmentServices";
 import Expertise from "@/components/home/expertise";
-
 import HeroSection from "@/components/home/heoSection";
 import OfferSection from "@/components/home/offer";
 import OurClients from "@/components/home/ourClients";
 import OurInfoSection from "@/components/home/ourInfo";
 import SevicesOffer from "@/components/home/servicesOffer";
-import Navbar from "@/components/navbar";
-
 export default function Home() {
   return (
     <div>
@@ -17,12 +14,10 @@ export default function Home() {
         <OurInfoSection />
         <DevelopmentServices />
         <OurClients />
-
       </div>
-      {/* <OfferSection /> */}
-      {/* <Expertise /> */}
-      {/* <SevicesOffer />
-   */}
+      <OfferSection />
+      <Expertise />
+      <SevicesOffer />
     </div>
-  )
+  );
 }
